ce9102db72fe5e39aecf9edb2e91e51a4fcfffd7
BrianHGrant <bhgrant8@gmail.com>
2016-06-14 23:54:32 -0700

    change on button to generate oscillator

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,16 @@
-function loadHalloweenSound() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'sounds/Halloween.mp3', true);
-  request.responseType = 'arraybuffer';
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      HalloweenBuffer = buffer;
-      playSound();
-    });
-  };
-  request.send();
-}
 
-function playSound(buffer) {
-  var source = context.createBufferSource();
-  source.buffer = buffer;
-  source.connect(context.destination);
-  source.start(0);
-}
 
 $(document).ready(function() {
   // Fix up prefixing
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  // oscillator = null;
+  oscillator = null;
 
   $("#onOff").click(function() {
     var context = new AudioContext();
-    // oscillator = context.createOscillator();
-    // oscillator.connect(context.destination);
-    // oscillator.start(context.currentTime);
-    loadHalloweenSound();
+    oscillator = context.createOscillator();
+    oscillator.connect(context.destination);
+    oscillator.start(context.currentTime);
   });
 
 
 });
-//   var HalloweenBuffer = null;
-//
-// function loadHalloweenSound(url) {
-//   var request = new XMLHttpRequest();
-//   request.open('GET', url, true);
-//   request.responseType = 'arraybuffer';
-//
-//   // Decode asynchronously
-//   request.onload = function() {
-//     context.decodeAudioData(request.response, function(buffer) {
-//       HalloweenBuffer = buffer;
-//     }, onError);
-//   }
-//   request.send();
-// }
-//
